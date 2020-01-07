958a2fe2f3304780e3386762d957d7099042cb72
sunshinehope <shankai2018@163.com>
2020-01-08 00:11:32 +0800

    ip

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask
 from flask import request
 
@@ -12,3 +13,6 @@ def get_couplet_down():
     couplet_down = up2down.get_down_couplet([couplet_up])
 
     return couplet_up + "," + couplet_down[0]
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
